a28e9157d2d777b54e5224212d9b2212e67e1629
Jan Šindler <jan.sindler@educhem.cz>
2022-03-28 13:45:57 +0200

    Přidejte soubory projektu.

---
+++ ukolbrezen/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ukolbrezen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cislo1 = Convert.ToInt32(Console.ReadLine());
+            int cislo2 = Convert.ToInt32(Console.ReadLine());
+            int max = Math.Max(cislo1, cislo2);
+            int min = Math.Min(cislo1, cislo2);
+            Console.WriteLine($"{max} > {min}");
+            Console.ReadKey();
+        }
+    }
+}
